e4b7a20920c601a5710da7c8d17adac66104d6cc
Anas <anastai245@gmail.com>
2026-03-08 12:25:03 +0530

    Initial commit

---
+++ src/main/java/com/lovify/app/LovifyApplication.java
@@ -0,0 +1,13 @@
+package com.lovify.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LovifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LovifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lovify/app/LovifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lovify.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LovifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
